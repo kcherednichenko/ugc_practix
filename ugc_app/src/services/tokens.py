644be07d9af82855d1c6b80f4406bc3c76962c7c
iKonstantin1991 <ikonstantin1991@mail.ru>
2024-07-21 17:24:14 +0300

    add bookmarks for users

--- ugc_app/src/services/tokens.py
+++ ugc_app/src/services/tokens.py
@@ -5,17 +5,17 @@ from pydantic import BaseModel, ValidationError
 import jwt
 
 from settings import settings
-from models.users import User
+from models.users import AuthenticatedUser
 
 _ALGORITHM = "RS256"
 
 
-def get_user_from_token(token: str) -> User | None:
+def get_user_from_token(token: str) -> AuthenticatedUser | None:
     try:
         payload = _decode_token(token)
     except (jwt.exceptions.InvalidTokenError, ValidationError):
         return None
-    return User(id=payload.user_id, roles=payload.roles)
+    return AuthenticatedUser(id=payload.user_id, roles=payload.roles)
 
 
 def _decode_token(token: str) -> "_TokenPayload":
